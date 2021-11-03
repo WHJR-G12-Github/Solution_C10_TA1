17aa8709d83c01c0cf8655057214c67c168e6a3b
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-11-03 12:53:55 +0530

    Update solution_c10_ta1.py

--- solution_c10_ta1.py
+++ solution_c10_ta1.py
@@ -12,24 +12,22 @@ groundx=0
 speed=0
 # Defining a function to make the bird move down
 def movedown():
-    # Declare the variable 'speed' as global
+    # Declaring the variable 'speed' as global
     global speed
-    # Create a variable 'gravity' and initialize it to 0.2
+    # Creating a variable 'gravity' and initialize it to 0.2
     gravity=0.2
-    # Increment 'speed' by 'gravity'
+    # Incrementing 'speed' by 'gravity'
     speed=speed+gravity
-    # Increment 'bird.y' by 'speed'
+    # Incrementing 'bird.y' by 'speed'
     bird.y=bird.y+speed
-def moveup():
-    global speed
-    speed=speed-10
+
 while True:  
   screen.blit(images["bg"],[0,0])
   groundx-=5
   if groundx<-550:
       groundx=0
   screen.blit(images["ground"],[groundx,550])
-  # Call the function 'movedown()' before displaying the bird image
+  # Calling the function 'movedown()' before displaying the bird image
   movedown()
   screen.blit(images["bird"],bird)
   
@@ -37,10 +35,7 @@ while True:
     if event.type==pygame.QUIT:
         pygame.quit()
   
-    if event.type==pygame.KEYDOWN:
-        if event.key==pygame.K_SPACE:
-            moveup()  
-  
+      
   pygame.display.update()
   
   pygame.time.Clock().tick(30)
